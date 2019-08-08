cd3b9c97f76cf7ab3f5f7b19d19fb5f60e7f3c55
rdfenix <rhoodsk8erboy@gmail.com>
2019-08-08 13:43:03 -0300

    front ended

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,76 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../assets/logo.svg'
 import like from '../assets/like.svg'
 import dislike from '../assets/dislike.svg'
 import './Main.css'
+import api from '../services/api'
 
 function Main(props) {
     const { match } = props
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: { user: match.params.id }
+            })
+
+            setUsers(response.data)
+        }
+
+        loadUsers()
+    }, [match.params.id])
+
+    async function handleLike(id) {
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: { user: match.params.id }
+        })
+
+        setUsers(users.filter(user => user._id !== id))
+    }
+
+    async function handleDislike(id) {
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: { user: match.params.id }
+        })
+
+        setUsers(users.filter(user => user._id !== id))
+    }
+
     return (
         <div className="main-container">
-            <img src={logo} alt="TinDev" />
-            <ul>
-                <li>
-                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
-                    <footer>
-                        <strong>Felipe Deschamps</strong>
-                        <p>Programador e cantor sertanejo nos tempos livres</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
-                    <footer>
-                        <strong>Felipe Deschamps</strong>
-                        <p>Programador e cantor sertanejo nos tempos livres</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
-                    <footer>
-                        <strong>Felipe Deschamps</strong>
-                        <p>Programador e cantor sertanejo nos tempos livres</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt="" />
-                    <footer>
-                        <strong>Felipe Deschamps</strong>
-                        <p>Programador e cantor sertanejo nos tempos livres</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-            </ul>
+            <Link to="/">
+                <img src={logo} alt="TinDev" />
+            </Link>
+            {
+                users.length > 0 ?
+                    <ul>
+                        {
+                            users.map(user => (
+                                <li key={user._id}>
+                                    <img src={user.avatar} alt={user.name} />
+                                    <footer>
+                                        <strong>{user.name}</strong>
+                                        <p>{user.bio}</p>
+                                    </footer>
+                                    <div className="buttons">
+                                        <button type="button" onClick={() => handleDislike(user._id)}>
+                                            <img src={dislike} alt="dislike" />
+                                        </button>
+                                        <button type="button" onClick={() => handleLike(user._id)}>
+                                            <img src={like} alt="like" />
+                                        </button>
+                                    </div>
+                                </li>
+
+                            ))
+                        }
+                    </ul>
+                    :
+                    <div className="empty">Is Empty :(</div>
+            }
         </div>
     )
 }
